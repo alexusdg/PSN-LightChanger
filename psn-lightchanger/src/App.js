ba42a3b4fda16bc7085f3f949859a7836c852573
Alexus Gore <alexusd12@gmail.com>
2024-07-13 00:12:44 -0400

    implementing the environment

---
+++ psn-lightchanger/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import currentlyPlaying from psn_functions
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {currentlyPlaying()}
+      </header>
+    </div>
+  );
+}
+
+export default App;
