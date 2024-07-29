885d27d4a7f34ef1ab989ef53c26b659023ddf58
Alexus Gore <alexusd12@gmail.com>
2024-07-29 01:11:32 -0400

    Working on the sub headers that will be used across the pages

--- psn-lightchanger/src/Components/interface.js
+++ psn-lightchanger/src/Components/interface.js
@@ -1,6 +1,16 @@
+import React from "react"
+import '../Style/style.css'
 
 
 export const Header = () => {
 
-    return (<div className='secondary-color' id='header'></div>)
+    return (<div className='secondary-color' id='header'>
+        PSN Light Changer
+    </div>)
+}
+
+
+export const subHeader = ({title}) => {
+
+    return (<div className='sub_section'>{title}</div>)
 }
--- psn-lightchanger/src/Pages/lights.js
+++ psn-lightchanger/src/Pages/lights.js
@@ -2,7 +2,8 @@ import { React, useState } from 'react'
 import { GetLights, IsAuth} from "../Functions/lifx_functions"
 import { userInfo } from 'os'
 import '../Style/style.css'
-import {Header} from '../Components/interface'
+import {Header } from '../Components/interface'
+import subHeader from '../Components/interface'
 
 // <input id='lifx_auth_code' type="text" defaultValue=""/>
 // <button type="button" value="submit" onClick={handleInput}/>
@@ -19,6 +20,9 @@ function LifxLights() {
 
     return(<div className='full_page primary-color'>
             <Header/> 
+                <div className='sub_section'>
+                    <subHeader title={"Testing"}/>
+                </div>
     </div>)
 }
 
