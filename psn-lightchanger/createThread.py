ae471306b4d67ea42f28afdb3883c41d52cc5d1e
Alexus Gore <alexusd12@gmail.com>
2024-12-09 08:45:11 -0500

    Removed some comments and print statements

--- psn-lightchanger/createThread.py
+++ psn-lightchanger/createThread.py
@@ -1,6 +1,4 @@
 from multiprocessing import Process
-import httpx
-import threading
 import requests
 import time
 import sys
@@ -22,8 +20,6 @@ def update_light_temp(token, psn_token, games_dict, light_id, tim):
         response = requests.get(psn_url)
         response = response.text
 
-        #print(games_dict)
-
         if response == {} or response == "{}":
             continue
 
@@ -33,10 +29,7 @@ def update_light_temp(token, psn_token, games_dict, light_id, tim):
         color = json.dumps(games_dict[f"{response}"])
 
         #This API requests prevents the while loop from working continuosly 
-        #with httpx.Client() as client:
-        post_url = f"http://localhost:3100/update_light/{token}/{light_id}/{color}"
-            #client.post(post_url)
-        
+        post_url = f"http://localhost:3100/update_light/{token}/{light_id}/{color}"        
        
         requests.put(post_url)
         time.sleep(tim)
@@ -56,10 +49,7 @@ def main():
     games_dict = json.load(file)
     json.dumps(games_dict)
                 
-    
-    print(games_dict)
     procs = []
-    #while True:
     for x in label:
         print(x)
         proc = Process(target=update_light_temp , args=(token, psn_token, games_dict, x, len(label)/LIFX_REFRESH_RATE))
