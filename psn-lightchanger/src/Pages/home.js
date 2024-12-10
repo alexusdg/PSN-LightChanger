fd459e280e966d96ad5e737cb4af51f705b901e0
Alexus Gore <alexusd12@gmail.com>
2024-12-09 22:44:46 -0500

    cleaned up home page

--- psn-lightchanger/src/Pages/home.js
+++ psn-lightchanger/src/Pages/home.js
@@ -1,21 +1,21 @@
-import {Welcome, Title, GetStartedButton} from "../Components/interface"
-
+import { Welcome, Title, GetStartedButton } from "../Components/interface"
 
 /**
- * 
+ *
  * @function Home will return an HTML of home layout
- * 
+ *
  * @returns an HTML
  */
-function Home(){
-
-    return(<div className='full_page' id='primary_color'>  
-                <div className="welcome_content">
-                        <Title/>
-                        <Welcome/>
-                        <GetStartedButton page={"/psn_verify"}/>
-                    </div>
-            </div>)
+function Home() {
+  return (
+    <div className="full_page" id="primary_color">
+      <div className="welcome_content">
+        <Title />
+        <Welcome />
+        <GetStartedButton page={"/psn_verify"} />
+      </div>
+    </div>
+  )
 }
 
 export default Home
