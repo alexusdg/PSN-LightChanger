a2f000c38b604d64a70083c9dd776845627a5d01
Alexus Gore <alexusd12@gmail.com>
2024-12-10 23:22:44 -0500

    Used pylint and updated variable names in create_process.py

--- psn-lightchanger/backend.js
+++ psn-lightchanger/backend.js
@@ -1,167 +1,143 @@
-const { spawn } = require('child_process')
-const cors = require('cors')
-const psnapi = require('psn-api')
-const axios = require('axios')
-const express = require('express'), bodyParser = require('body-parser');
+const { spawn } = require("child_process")
+const cors = require("cors")
+const psnapi = require("psn-api")
+const axios = require("axios")
+const express = require("express"),
+  bodyParser = require("body-parser")
 const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-    throw new Error('BROKEN');
-});
+app.get("/", (req, res, next) => {
+  throw new Error("BROKEN")
+})
 
 /**
  * @api /psinfo is used to gather psn user information
- * 
+ *
  * @returns a json contain psn user info upon success otherwise
  *          it will return nothing
  */
 app.get("/psinfo/:npsso", (req, res, next) => {
+  const npssoCookie = req.params.npsso
 
-    const npssoCookie = req.params.npsso
+  async function playerInfo() {
+    const myNpsso = npssoCookie
 
-    //console.log(npssoCookie)
+    try {
+      const accessCode = await psnapi.exchangeNpssoForCode(`${myNpsso}`)
 
-    async function playerInfo() {
-        
-        const myNpsso = npssoCookie
+      const authorization = await psnapi.exchangeCodeForAccessToken(accessCode)
 
-        try{
-
-            const accessCode = await psnapi.exchangeNpssoForCode(`${myNpsso}`)
-
-            const authorization = await psnapi.exchangeCodeForAccessToken(accessCode)
-
-            //const response = await psnapi.getBasicPresence(authorization, "me") 
-
-            console.log(authorization.refreshToken)
-
-            res.send({"access_code" : authorization.refreshToken})
-        }
-        catch(err){
-
-            res.status(500).send({ err })
-        }
+      res.send({ access_code: authorization.refreshToken })
+    } catch (err) {
+      res.status(500).send({ err })
     }
-    
-    playerInfo()
+  }
+
+  playerInfo()
 })
 
 app.get("/ps_game_playing/:access_code", (req, res) => {
+  const code = req.params.access_code //this is the refresh token
 
-    const code = req.params.access_code //this is refreshtokedn
+  async function currentGame() {
+    try {
+      const authorization = await psnapi.exchangeRefreshTokenForAuthTokens(code)
 
-    async function currentGame(){
+      const response = await psnapi.getBasicPresence(authorization, "me")
 
-        try{
-            //console.log(code, "HERE")
-
-            const authorization = await psnapi.exchangeRefreshTokenForAuthTokens(code)
-
-            //console.log(authorization)
-
-            const response = await psnapi.getBasicPresence(authorization, "me")
-
-            //console.log(response)
-
-            res.send(response.basicPresence.gameTitleInfoList[0].titleName)
-
-        }catch(err){
-
-
-            res.status(500).send(err)
-
-        }
-
+      res.send(response.basicPresence.gameTitleInfoList[0].titleName)
+    } catch (err) {
+      res.status(500).send(err)
     }
+  }
 
-    currentGame()
+  currentGame()
 })
 
-
 app.put("/create_process/:label", (req, res) => {
+  //console.log("In create Thread")
+  const authToken = "Bearer ".concat(req.body.data.lifx_token)
+  const psn_token = req.body.data.psn_token
 
-    console.log(req.params.label)
-    console.log("In create Thread")
-    const authToken = 'Bearer '.concat(req.body.data.lifx_token)
-    const psn_token = req.body.data.psn_token
+  const pyProg = spawn(
+    "python",
+    ["create_process.py"].concat(
+      req.body.data.lifx_token,
+      psn_token,
+      req.params.label,
+    ),
+  )
 
+  // Collect data from script and print to console
+  var data = ""
+  pyProg.stdout.on("data", (stdout) => {
+    data += stdout.toString()
+  })
 
-    const pyProg = spawn('python', ['createProcess.py'].concat(req.body.data.lifx_token, psn_token, req.params.label));
+  // Print errors to console, if any
+  pyProg.stderr.on("data", (stderr) => {
+    console.log(`stderr: ${stderr}`)
+  })
 
-    // Collect data from script and print to console
-    var data = ''
-    pyProg.stdout.on('data', (stdout) => {
-        data += stdout.toString()
-    });
+  // When script is finished, print collected data
+  pyProg.on("close", (code) => {
+    console.log(`child process exited with code ${code}`)
+    console.log(data)
+  })
 
-    // Print errors to console, if any
-    pyProg.stderr.on('data', (stderr) => {
-        console.log(`stderr: ${stderr}`)
-    });
-
-    // When script is finished, print collected data
-    pyProg.on('close', (code) => {
-        console.log(`child process exited with code ${code}`)
-        console.log(data)
-    });
-
-    return res.status(200)
-
+  return res.status(200)
 })
 
 app.get("/light_color/:token/:id", (req, res) => {
+  const authToken = req.params.token
+  const id = req.params.id
 
-    const authToken = req.params.token
-    const id = req.params.id
+  axios
+    .get(`https://api.lifx.com/v1/lights/${id}`, {
+      headers: {
+        Authorization: authToken,
+      },
+    })
+    .then((response) => {
 
-    axios.get(`https://api.lifx.com/v1/lights/${id}`, {
-        headers: {
-            "Authorization" : authToken
-        }
-    }).then((response) => {
-
-        console.log(response.data[0].color)
-
-        res.status(200).send({"color" : response.data[0].color})
+      res.status(200).send({ color: response.data[0].color })
     })
 })
 
 app.put("/update_light/:token/:id/:color_req/", (req, res) => {
+  const authToken = "Bearer ".concat(req.params.token)
+  var id = req.params.id
+  const state = req.data
 
-    const authToken = 'Bearer '.concat(req.params.token)
-    var id = req.params.id
-    const state = req.data
+  color_req = JSON.parse(req.params.color_req)
 
-    color_req = JSON.parse(req.params.color_req)
+  id = JSON.parse(id)
 
-    console.log(color_req)
+  const options = {
+    method: "PUT",
+    url: `https://api.lifx.com/v1/lights/${id}/state`,
+    headers: {
+      accept: "text/plain",
+      "content-type": "application/json",
+      Authorization: authToken,
+    },
+    data: {
+      color: `hue:${color_req.hue} saturation:${color_req.saturation} kelvin:${color_req.kelvin}`,
+      duration: "1",
+    }
+  }
 
-    id = JSON.parse(id)
+  axios
+    .request(options)
+    .then((res) => {})
+    .catch((err) => console.error(err))
 
-    const options = {
-        method: 'PUT',
-        url: `https://api.lifx.com/v1/lights/${id}/state`,
-        headers: {
-          accept: 'text/plain',
-          'content-type': 'application/json',
-          Authorization: authToken
-        },
-        data: {"color" : `hue:${color_req.hue} saturation:${color_req.saturation} kelvin:${color_req.kelvin}`,
-                "duration" : "0.5"    
-            }
-      };
-      
-      axios
-        .request(options)
-        .then(res => console.log(res.data))
-        .catch(err => console.error(err))
-
-    res.status(200).send("ok")
+  res.status(200).send("ok")
 })
 
 app.listen(3100, () => {
-    console.log("Server is Running")
+  console.log("Server is Running")
 })
