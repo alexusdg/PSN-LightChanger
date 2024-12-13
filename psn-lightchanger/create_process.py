6b39c5f341df6c4981b1101111de213724879640
Alexus Gore <alexusd12@gmail.com>
2024-12-12 21:49:09 -0500

    updated create_process function descriptons

--- psn-lightchanger/create_process.py
+++ psn-lightchanger/create_process.py
@@ -13,7 +13,23 @@ LIFX_REQUESTS = 120
 LIFX_SECONDS = 60
 LIFX_REFRESH_RATE = LIFX_REQUESTS/LIFX_SECONDS
 
+
 def update_light_temp(lifx_token, psn_token, games_dict, light_id, nap_time):
+    '''
+    Runs a while loop to make the requests to determine the game currently 
+    being played and update the color of the lifx light
+    
+    Args:
+        lifx_token (string) : lifx access token
+        psn_token (string) : psn refresh token
+        games_dict (dict) : key-value pairs, games and their respective color info
+        light_id (string) : Lifx light id
+        nap_time (number) : Lifx refresh rate ratio 
+
+    Returns:
+        nothing
+    '''
+
     t_end = time.time() + 60 * 0.5
     while time.time() < t_end:
 
@@ -41,7 +57,19 @@ def update_light_temp(lifx_token, psn_token, games_dict, light_id, nap_time):
         time.sleep(nap_time)
 
 def main():
-    token = sys.argv[1]
+    '''
+    Will gather the arguments and create and and start the processes to update the lights
+
+    Args :  
+        lifx_token (string) : lifx access token
+        psn_token (string) : psn refresh token
+        light_ids (array) : string array containg light ids
+        
+    Returns:     
+        nothing
+    '''
+
+    lifx_token = sys.argv[1]
     psn_token = str(sys.argv[2])
     light_ids = sys.argv[3]
 
@@ -54,7 +82,7 @@ def main():
     procs = []
     for id in light_ids:
         proc = Process(target=update_light_temp,
-                       args=(token,
+                       args=(lifx_token,
                              psn_token,
                              games_dict,
                              id,
